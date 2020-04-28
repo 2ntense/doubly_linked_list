200b03bf649f05f1a15ba62bb5f5cf03fb4fa71b
2ntense <stefanyip@hotmail.com>
2020-04-28 04:41:20 +0200

    added dll_insert test

--- test/check_linkedlist.c
+++ test/check_linkedlist.c
@@ -126,6 +126,67 @@ START_TEST(test_dll_prepend)
 }
 END_TEST
 
+START_TEST(test_dll_insert)
+{
+    list_t *test_list;
+    test_list = create_list();
+    ck_assert_ptr_ne(test_list, NULL);
+
+    node_t *n0 = dll_insert(0, 111, test_list);
+    ck_assert_ptr_ne(n0, NULL);
+    ck_assert_int_eq(test_list->size, 1);
+    ck_assert_ptr_eq(test_list->head, n0);
+    ck_assert_ptr_eq(test_list->tail, n0);
+    ck_assert_int_eq(n0->data, 111);
+    ck_assert_ptr_eq(n0->next, NULL);
+    ck_assert_ptr_eq(n0->prev, NULL);
+
+    node_t *head_node = dll_insert(0, 222, test_list);
+    ck_assert_ptr_eq(test_list->head, head_node);
+    ck_assert_ptr_eq(test_list->tail, n0);
+    ck_assert_int_eq(test_list->size, 2);
+    ck_assert_int_eq(head_node->data, 222);
+    ck_assert_ptr_eq(head_node->prev, NULL);
+    ck_assert_ptr_eq(head_node->next, n0);
+    ck_assert_int_eq(n0->data, 111);
+    ck_assert_ptr_eq(n0->prev, head_node);
+    ck_assert_ptr_eq(n0->next, NULL);
+
+    node_t *n1 = dll_insert(1, 333, test_list);
+    ck_assert_int_eq(test_list->size, 3);
+    ck_assert_ptr_eq(test_list->head, head_node);
+    ck_assert_ptr_eq(test_list->tail, n0);
+    ck_assert_int_eq(head_node->data, 222);
+    ck_assert_ptr_eq(head_node->prev, NULL);
+    ck_assert_ptr_eq(head_node->next, n1);
+    ck_assert_int_eq(n0->data, 111);
+    ck_assert_ptr_eq(n0->prev, n1);
+    ck_assert_ptr_eq(n0->next, NULL);
+    ck_assert_int_eq(n1->data, 333);
+    ck_assert_ptr_eq(n1->next, n0);
+    ck_assert_ptr_eq(n1->prev, head_node);
+
+    node_t *tail_node = dll_insert(3, 444, test_list);
+    ck_assert_int_eq(test_list->size, 4);
+    ck_assert_ptr_eq(test_list->head, head_node);
+    ck_assert_ptr_eq(test_list->tail, tail_node);
+    ck_assert_int_eq(head_node->data, 222);
+    ck_assert_ptr_eq(head_node->prev, NULL);
+    ck_assert_ptr_eq(head_node->next, n1);
+    ck_assert_int_eq(n0->data, 111);
+    ck_assert_ptr_eq(n0->prev, n1);
+    ck_assert_ptr_eq(n0->next, tail_node);
+    ck_assert_int_eq(n1->data, 333);
+    ck_assert_ptr_eq(n1->next, n0);
+    ck_assert_ptr_eq(n1->prev, head_node);
+    ck_assert_int_eq(tail_node->data, 444);
+    ck_assert_ptr_eq(tail_node->next, NULL);
+    ck_assert_ptr_eq(tail_node->prev, n0);
+
+    dll_free(test_list);
+}
+END_TEST
+
 Suite *linkedlist_suite()
 {
     Suite *s;
@@ -138,6 +199,7 @@ Suite *linkedlist_suite()
     tcase_add_test(tc_core, test_dll_pop);
     tcase_add_test(tc_core, test_dll_clear);
     tcase_add_test(tc_core, test_dll_prepend);
+    tcase_add_test(tc_core, test_dll_insert);
 
     suite_add_tcase(s, tc_core);
 
